d7b139f716e911b246e7c80d1f4ad2461db7b4b4
Javier Jimenez <javierjimenez@Accounts-MBP.lan>
2021-05-31 01:00:56 -0700

    Working on Specifications for the Assignment, steps 1 - 7.

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,14 +1,13 @@
 class OrdersController < ApplicationController
   rescue_from ActiveRecord::RecordNotFound, with: :catch_not_found
   before_action :set_order, only: [:show, :edit, :update, :destroy]
-  layout 'order_layout'
 
   # GET /order/new
   def new
     @order = Order.new
   end
 
-  # GET /oders
+  # GET /orders
   # GET /orders.json
   def index
     @orders = Order.all
@@ -26,30 +25,18 @@ class OrdersController < ApplicationController
   # POST /orders
   # POST /orders.json
   def create
-    # @customer = Customer.new(customer_params)
-    #
-    # respond_to do |format|
-    #   if @customer.save
-    #     format.html { redirect_to @customer, notice: 'Customer was successfully created.' }
-    #     format.json { render :show, status: :created, location: @customer }
-    #   else
-    #     format.html { render :new }
-    #     format.json { render json: @customer.errors, status: :unprocessable_entity }
-    #   end
-    # end
-
-    # @customer = Customer.new(customer_params)
-    # @customer.save
-    # flash.notice = "The customer record was created successfully."
-    # redirect_to @customer
+    # @order = Order.new(order_params)
+    # @order.save
+    # flash.notice = "The order record was created successfully."
+    # redirect_to @order
 
     # create Method with Error Handling:
-    # If @customer.save return non-nil values, that means they succeeded,
+    # If @order.save return non-nil values, that means they succeeded,
     # and we can redirect back to the show page with a success message.
     # If they return nil, we have the else processing:
     # First collect the error and put it in the flash.now.alert
-    # Then do the render, which basically means put the previous screen back up.
-    @order = Order.new(customer_params)
+    # Then do the render to :new.
+    @order = Order.new(order_params)
     if @order.save
       flash.notice = "The order record was created successfully."
       redirect_to @order
@@ -62,26 +49,16 @@ class OrdersController < ApplicationController
   # PATCH/PUT /orders/1
   # PATCH/PUT /orders/1.json
   def update
-    # respond_to do |format|
-    #   if @customer.update(customer_params)
-    #     format.html { redirect_to @customer, notice: 'Customer was successfully updated.' }
-    #     format.json { render :show, status: :ok, location: @customer }
-    #   else
-    #     format.html { render :edit }
-    #     format.json { render json: @customer.errors, status: :unprocessable_entity }
-    #   end
-    # end
-
-    # @customer.update(customer_params)
-    # flash.notice = "The customer record was updated successfully."
-    # redirect_to @customer
+    # @order.update(order_params)
+    # flash.notice = "The order record was updated successfully."
+    # redirect_to @order
 
     # update Method with Error Processing
-    # If @customer.update return non-nil values, that means they succeeded,
+    # If @order.update return non-nil values, that means they succeeded,
     # and we can redirect back to the show page with a success message.
     # If they return nil, we have the else processing:
     # First collect the error and put it in the flash.now.alert
-    # Then do the render, which basically means put the previous screen back up.
+    # Then do the render to :edit.
     if @order.update(order_params)
       flash.notice = "The order record was updated successfully."
       redirect_to @order
@@ -104,17 +81,17 @@ class OrdersController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_order
-      @order = Order.find(params[:id])
+      @order = Order.find(:customer_id)
     end
 
     # Only allow a list of trusted parameters through.
     def order_params
-      params.require(:order).permit(:product_name, :product_count, :customer)
+      params.require(:order).permit(:customer_id, :product_name, :product_count)
     end
 
     def catch_not_found(e)
       Rails.logger.debug("We had a not found exception.")
       flash.alert = e.to_s
-      redirect_to orders_path
+      redirect_to orders_new_path
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ Rails.application.routes.draw do
   get 'orders/edit'
 
   root to: 'customers#index' # this will configure server to bring up index page when connecting to root path
+  #root to: 'orders#index' # this will configure server to bring up index page when connecting to root path
   resources :customers
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
